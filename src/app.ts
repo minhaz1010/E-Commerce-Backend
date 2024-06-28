9979c71ccf1f0d66036a806533bfa31aea4e85cc
minhaz1010 <minhazrahman1010@gmail.com>
2024-06-28 20:32:34 +0600

    feat: add logger (morgan)

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,12 @@ import cors from "cors";
 import globalErrorHandler from "./app/middleware/globalErrorHandler";
 import notFound from "./app/middleware/notFound";
 import router from "./app/route";
+import morgan from "morgan";
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(morgan("dev"));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello dear");
